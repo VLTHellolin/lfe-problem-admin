7dc34091356cdd6ccdcf715abe0a728b2b8747c9
VLTHellolin <i@hellolin.top>
2024-09-16 18:35:29 +0800

    build: Use webpack-userscript

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,20 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { monkey } from 'webpack-monkey';
 import terser from 'terser-webpack-plugin';
+import { UserscriptPlugin } from 'webpack-userscript';
+import fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
-export default monkey({
+export default {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'lfe-problem-admin.user.js',
-  },
-  externals: {
-    jquery: 'https://cdn.hellolin.top/npm/jquery@3.7.1/dist/jquery.min.js',
-    localforage:
-      'https://cdn.hellolin.top/npm/localforage@1.10.0/dist/localforage.min.js',
+    filename: 'lfe-problem-admin.js',
   },
   resolve: {
-    extensions: ['.ts', '.json', '...'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: 'ts-loader',
-      },
-    ],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   optimization: {
     minimize: true,
@@ -38,9 +26,48 @@ export default monkey({
       }),
     ],
   },
-  monkey: {
-    meta: {
-      resolve: () => path.resolve('./src/meta.ts'),
-    },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
-});
+  externals: {
+    jquery: '$',
+    localforage: 'localforage',
+  },
+  plugins: [
+    new UserscriptPlugin({
+      headers: {
+        name: pkg.name,
+        description: pkg.description,
+        author: pkg.author,
+        version: pkg.version,
+        license: pkg.license,
+        connect: ['www.luogu.com.cn'],
+        match: [
+          '*://www.luogu.com.cn/problem*',
+          '*://www.luogu.com.cn/sadmin/article/review*',
+        ],
+        require: [
+          'https://cdn.hellolin.top/npm/jquery@3.7.1/dist/jquery.min.js',
+          'https://cdn.hellolin.top/npm/localforage@1.10.0/dist/localforage.min.js',
+        ],
+        resource: [
+          'tagsData  https://cdn.hellolin.top/gh/VLTHellolin/lfe-problem-admin/src/data/tags.json',
+        ],
+        grant: ['GM_getResourceText'],
+        updateURL:
+          'https://github.com/VLTHellolin/lfe-problem-admin/releases/latest/download/lfe-problem-admin.user.js',
+        downloadURL:
+          'https://github.com/VLTHellolin/lfe-problem-admin/releases/latest/download/lfe-problem-admin.user.js',
+      },
+      pretty: true,
+      strict: true,
+      whitelist: true,
+    }),
+  ],
+};
