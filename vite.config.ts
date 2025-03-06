9424a69da769d607aaa2630637c2d4a04712093f
VLTHellolin <i@hellolin.top>
2025-03-06 13:27:51 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,16 @@ import monkey from 'vite-plugin-monkey';
 import pkg from './package.json';
 
 export default defineConfig({
-  build: {
-    minify: true,
-  },
+  build: { minify: true },
   plugins: [
     reactPlugin(),
     monkey({
       entry: 'src/index.ts',
       build: {
         externalGlobals: {
-          clsx: ['clsx', 'https://cdn.hellolin.top/npm/clsx@2.1.1/dist/clsx.min.js'],
-          idb: ['idb', 'https://cdn.hellolin.top/npm/idb@8.0.1/build/umd.js'],
-          react: ['React', 'https://cdn.hellolin.top/npm/react@18.3.1/umd/react.production.min.js'],
+          'react': ['React', 'https://cdn.hellolin.top/npm/react@18.3.1/umd/react.production.min.js'],
           'react-dom': ['ReactDOM', 'https://cdn.hellolin.top/npm/react-dom@18.3.1/umd/react-dom.production.min.js'],
-          sweetalert2: ['Swal', 'https://cdn.hellolin.top/npm/sweetalert2@11.15.10/dist/sweetalert2.all.min.js'],
+          'sweetalert2': ['Swal', 'https://cdn.hellolin.top/npm/sweetalert2@11.17.2/dist/sweetalert2.all.min.js'],
         },
       },
       userscript: {
